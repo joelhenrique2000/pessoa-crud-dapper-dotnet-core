2941b578888a3b82255afa38fe1e39ccc9c7dc73
Joel Henrique Silva Santos <joelhenrique2000@gmail.com>
2020-10-17 22:55:03 -0300

    Criando a classe de conexão com o banco

---
+++ pessoa-crud/DbConnection.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pessoa_crud {
+    public interface IDbConnection {
+        public SqlConnection GetConnection { get; }
+    }
+    public class DbConnection : IDbConnection {
+        IConfiguration Configuration;
+
+        public DbConnection(IConfiguration configuration) {
+            Configuration = configuration;
+        }
+
+        public SqlConnection GetConnection {
+            get {
+                var connectionString = Configuration.GetConnectionString("AuthDbContextConnection");
+                return new SqlConnection(connectionString);
+            }
+        }
+    }
+}
