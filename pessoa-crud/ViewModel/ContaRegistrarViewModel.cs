397dab648b2b0b3315b1d333f21da930c158699b
Joel Henrique Silva Santos <joelhenrique2000@gmail.com>
2020-10-18 21:18:03 -0300

    Criado o ViewModel e a view de cadastrar usuário

---
+++ pessoa-crud/ViewModel/ContaRegistrarViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pessoa_crud.ViewModel
+{
+    public class ContaRegistrarViewModel
+    {
+        [Required(ErrorMessage = "A senha é obrigatório")]
+        public string Senha { get; set; }
+
+        [Required(ErrorMessage = "Confirme sua senha")]
+        public string ConfirmarSenha { get; set; }
+
+        [Required(ErrorMessage = "O email é obrigatório")]
+        public string Email { get; set; }
+
+    }
+}
