5a536bc2fb5adf7cf7e1080b40d74a5199480ac7
Joel Henrique Silva Santos <joelhenrique2000@gmail.com>
2020-10-19 17:37:39 -0300

    Criado o ViewModel e a View de 'Entrar'

---
+++ pessoa-crud/ViewModel/ContaEntrarViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pessoa_crud.ViewModel
+{
+    public class ContaEntrarViewModel
+    {
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "A senha é obrigatório")]
+        public string Senha { get; set; }
+
+        [EmailAddress]
+        [Required(ErrorMessage = "O email é obrigatório")]
+        public string Email { get; set; }
+
+    }
+}
