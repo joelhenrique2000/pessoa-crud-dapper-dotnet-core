95d21dc1fe685612bc1462981207d188e566ab3a
Joel Henrique Silva Santos <joelhenrique2000@gmail.com>
2020-10-19 12:24:14 -0300

    Adicionando validação no ViewModel ContaRegistrar

--- pessoa-crud/ViewModel/ContaRegistrarViewModel.cs
+++ pessoa-crud/ViewModel/ContaRegistrarViewModel.cs
@@ -8,12 +8,17 @@ namespace pessoa_crud.ViewModel
 {
     public class ContaRegistrarViewModel
     {
+        [DataType(DataType.Password)]
         [Required(ErrorMessage = "A senha é obrigatório")]
         public string Senha { get; set; }
 
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirme sua senha")]
+        [Compare("Senha", ErrorMessage = "As senhas não conferem")]
         [Required(ErrorMessage = "Confirme sua senha")]
         public string ConfirmarSenha { get; set; }
 
+        [EmailAddress]
         [Required(ErrorMessage = "O email é obrigatório")]
         public string Email { get; set; }
 
