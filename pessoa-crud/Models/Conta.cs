9eb6fd25625611edbf09b1ba3f428342fcbb58be
Joel Henrique Silva Santos <joelhenrique2000@gmail.com>
2020-10-18 22:47:48 -0300

    Criado os models Conta e Identity

---
+++ pessoa-crud/Models/Conta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pessoa_crud.Models
+{
+    public class Conta
+    {
+        public string Id { get; set; }
+        public string Senha { get; set; }
+        public string ConfirmarSenha { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ pessoa-crud/Models/IdentityModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pessoa_crud.Models {
+    public class IdentityModels {
+    }
+
+    public class ApplicationUserRole : IdentityRole { }
+
+    public class ApplicationUser : IdentityUser { }
+}
+
